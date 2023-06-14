130dafb0b1b3b965ef37e237df7da5dc3d3f6b31
valer <valeriybv@gmail.com>
2023-06-14 22:23:58 +0300

    2

--- Main.py
+++ Main.py
@@ -2,13 +2,14 @@
 #Напишите код, который проверяет какая из этих строк длиннее.
 def compare_strings (str1, str2):
     if str1 < str2:
-        #void
+        print("1")
     elif str1 > str2:
-        #void
+        print("3")
     else:
-        #void
+        print("fdsfs")
 
 if __name__ == "__main__":
+    print ("dfdsfds")
     phrase1 = input("Введите строку 1:\n")
     phrase2 = input("Введите строку 2:\n")
     compare_strings(phrase1, phrase2)
