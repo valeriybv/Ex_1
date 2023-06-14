25d1268c136b6e1fe42431ef98630fe7dadb0648
valer <valeriybv@gmail.com>
2023-06-14 23:51:22 +0300

    +задание 2

--- strings.py
+++ strings.py
@@ -1,6 +1,8 @@
 #Даны 2 переменных, в которых хранятся строки произвольной длины: phrase_1 и phrase_2
 #Напишите код, который проверяет какая из этих строк длиннее.
-def compare_strings (str1, str2):
+def compare_strings ():
+    str1 = input("Введите строку 1:\n")
+    str2 = input("Введите строку 2:\n")
     if len(str1) < len(str2):
         print("Строка 1 короче строки 2")
     elif len(str1) > len(str2):
@@ -8,8 +10,7 @@ def compare_strings (str1, str2):
     else:
         print("Строки одинаковы по длине")
 
-if __name__ == "__main__":
-    print ("dfdsfds")
-    phrase1 = input("Введите строку 1:\n")
-    phrase2 = input("Введите строку 2:\n")
-    compare_strings(phrase1, phrase2)
+
+compare_strings()
+
+
