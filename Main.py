3a6b5067b81956fcbefd1421a7c6f16bbdb3581b
valer <valeriybv@gmail.com>
2023-06-14 22:22:40 +0300

    2

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
-test
+#Даны 2 переменных, в которых хранятся строки произвольной длины: phrase_1 и phrase_2
+#Напишите код, который проверяет какая из этих строк длиннее.
+def compare_strings (str1, str2):
+    if str1 < str2:
+        #void
+    elif str1 > str2:
+        #void
+    else:
+        #void
+
+if __name__ == "__main__":
+    phrase1 = input("Введите строку 1:\n")
+    phrase2 = input("Введите строку 2:\n")
+    compare_strings(phrase1, phrase2)
